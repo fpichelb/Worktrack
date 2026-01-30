817d7e49c1a56827afe5a38368316018ce4f13e4
Felix Pichelbauer <Felix@Pichelbauer.de>
2026-01-30 14:49:17 +0100

    ScrollUpButton /Scroll down bei button presses

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,3 +6,31 @@ window.downloadFile = (url, filename) => {
     a.click();
     document.body.removeChild(a);
 };
+window.backToTop = (function () {
+    let handler = null;
+  
+    function init(dotnetRef, threshold) {
+      threshold = threshold ?? 300;
+  
+      handler = () => {
+        const y = window.scrollY || document.documentElement.scrollTop || 0;
+        dotnetRef.invokeMethodAsync("SetBackToTopVisible", y > threshold);
+      };
+  
+      window.addEventListener("scroll", handler, { passive: true });
+      handler();
+    }
+  
+    function scrollTop() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  
+    function dispose() {
+      if (handler) {
+        window.removeEventListener("scroll", handler);
+        handler = null;
+      }
+    }
+  
+    return { init, scrollTop, dispose };
+  })();
